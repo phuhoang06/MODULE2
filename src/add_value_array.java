bb3445ec766e1796e125fad48dff1b79cd50ea22
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-11-06 22:26:01 +0700

    add new array

---
+++ src/add_value_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class add_value_array {
+    public static void main(String[] args) {
+        int size;
+        int[] arr;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter the size of the array : ");
+            size = sc.nextInt();
+            arr = new int[size];
+            if (size > 20) {
+                System.out.println("Nhap so luong nho hon");
+            }
+        } while (size > 20);
+        int i = 0;
+        while (i < arr.length) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+            i++;
+        }
+    }
+}
