c3a1c282885c2461c00fc5eb9908271008e5b927
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-11-11 10:49:50 +0700

    update 1

---
+++ src/access_modifier/Car.java
@@ -0,0 +1,13 @@
+package access_modifier;
+
+public class Car {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+
+}
