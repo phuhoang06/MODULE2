8ba07088ca5655bf68007672fc0ce95c38861189
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-11-11 13:49:45 +0700

    update Bt

---
+++ src/access_modifier/BT_Run_Student.java
@@ -0,0 +1,11 @@
+package access_modifier;
+
+import java.util.Scanner;
+
+public class BT_Run_Student {
+    public static void main(String[] args) {
+        BT_Student student = new BT_Student();
+        student.setName("alice");
+        student.setClasses("C09");
+    }
+}
---
+++ src/access_modifier/BT_Student.java
@@ -0,0 +1,17 @@
+package access_modifier;
+
+public class BT_Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public BT_Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
